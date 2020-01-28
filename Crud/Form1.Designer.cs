2941abe863de8676cf15487c14396ff646ea3046
luan-css <sluan298@gmail.com>
2020-01-28 08:02:49 -0300

    Meu crud

---
+++ Crud/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Crud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbNovo = new System.Windows.Forms.ToolStripButton();
+            this.tsbSalvar = new System.Windows.Forms.ToolStripButton();
+            this.tsbCancelar = new System.Windows.Forms.ToolStripButton();
+            this.tsbAlterar = new System.Windows.Forms.ToolStripButton();
+            this.tsbExcluir = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tstIdBuscar = new System.Windows.Forms.ToolStripTextBox();
+            this.tsbPesquisar = new System.Windows.Forms.ToolStripButton();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtUf = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.lblUf = new System.Windows.Forms.Label();
+            this.mskCep = new System.Windows.Forms.MaskedTextBox();
+            this.mskTel = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.funcionariosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.luanDataSet = new Crud.LuanDataSet();
+            this.funcionariosTableAdapter = new Crud.LuanDataSetTableAdapters.FuncionariosTableAdapter();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.funcionariosBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.luanDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbNovo,
+            this.tsbSalvar,
+            this.tsbCancelar,
+            this.tsbAlterar,
+            this.tsbExcluir,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.tstIdBuscar,
+            this.tsbPesquisar});
+            resources.ApplyResources(this.toolStrip1, "toolStrip1");
+            this.toolStrip1.Name = "toolStrip1";
+            // 
+            // tsbNovo
+            // 
+            this.tsbNovo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.tsbNovo, "tsbNovo");
+            this.tsbNovo.Name = "tsbNovo";
+            this.tsbNovo.Click += new System.EventHandler(this.tsbNovo_Click);
+            // 
+            // tsbSalvar
+            // 
+            this.tsbSalvar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.tsbSalvar, "tsbSalvar");
+            this.tsbSalvar.Name = "tsbSalvar";
+            this.tsbSalvar.Click += new System.EventHandler(this.tsbSalvar_Click);
+            // 
+            // tsbCancelar
+            // 
+            this.tsbCancelar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.tsbCancelar, "tsbCancelar");
+            this.tsbCancelar.Name = "tsbCancelar";
+            this.tsbCancelar.Click += new System.EventHandler(this.tsbCancelar_Click);
+            // 
+            // tsbAlterar
+            // 
+            this.tsbAlterar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.tsbAlterar, "tsbAlterar");
+            this.tsbAlterar.Name = "tsbAlterar";
+            this.tsbAlterar.Click += new System.EventHandler(this.tsbAlterar_Click);
+            // 
+            // tsbExcluir
+            // 
+            this.tsbExcluir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.tsbExcluir, "tsbExcluir");
+            this.tsbExcluir.Name = "tsbExcluir";
+            this.tsbExcluir.Click += new System.EventHandler(this.tsbExcluir_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            resources.ApplyResources(this.toolStripLabel1, "toolStripLabel1");
+            // 
+            // tstIdBuscar
+            // 
+            resources.ApplyResources(this.tstIdBuscar, "tstIdBuscar");
+            this.tstIdBuscar.Name = "tstIdBuscar";
+            // 
+            // tsbPesquisar
+            // 
+            this.tsbPesquisar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.tsbPesquisar, "tsbPesquisar");
+            this.tsbPesquisar.Name = "tsbPesquisar";
+            this.tsbPesquisar.Click += new System.EventHandler(this.tsbPesquisar_Click);
+            // 
+            // lblId
+            // 
+            resources.ApplyResources(this.lblId, "lblId");
+            this.lblId.Name = "lblId";
+            // 
+            // txtId
+            // 
+            resources.ApplyResources(this.txtId, "txtId");
+            this.txtId.Name = "txtId";
+            // 
+            // txtEndereco
+            // 
+            resources.ApplyResources(this.txtEndereco, "txtEndereco");
+            this.txtEndereco.Name = "txtEndereco";
+            // 
+            // txtBairro
+            // 
+            resources.ApplyResources(this.txtBairro, "txtBairro");
+            this.txtBairro.Name = "txtBairro";
+            // 
+            // txtCidade
+            // 
+            resources.ApplyResources(this.txtCidade, "txtCidade");
+            this.txtCidade.Name = "txtCidade";
+            // 
+            // txtUf
+            // 
+            resources.ApplyResources(this.txtUf, "txtUf");
+            this.txtUf.Name = "txtUf";
+            // 
+            // txtNome
+            // 
+            resources.ApplyResources(this.txtNome, "txtNome");
+            this.txtNome.Name = "txtNome";
+            // 
+            // lblNome
+            // 
+            resources.ApplyResources(this.lblNome, "lblNome");
+            this.lblNome.Name = "lblNome";
+            // 
+            // lblEndereco
+            // 
+            resources.ApplyResources(this.lblEndereco, "lblEndereco");
+            this.lblEndereco.Name = "lblEndereco";
+            // 
+            // lblBairro
+            // 
+            resources.ApplyResources(this.lblBairro, "lblBairro");
+            this.lblBairro.Name = "lblBairro";
+            // 
+            // lblCidade
+            // 
+            resources.ApplyResources(this.lblCidade, "lblCidade");
+            this.lblCidade.Name = "lblCidade";
+            // 
+            // lblUf
+            // 
+            resources.ApplyResources(this.lblUf, "lblUf");
+            this.lblUf.Name = "lblUf";
+            // 
+            // mskCep
+            // 
+            resources.ApplyResources(this.mskCep, "mskCep");
+            this.mskCep.Name = "mskCep";
+            // 
+            // mskTel
+            // 
+            resources.ApplyResources(this.mskTel, "mskTel");
+            this.mskTel.Name = "mskTel";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // funcionariosBindingSource
+            // 
+            this.funcionariosBindingSource.DataMember = "Funcionarios";
+            this.funcionariosBindingSource.DataSource = this.luanDataSet;
+            // 
+            // luanDataSet
+            // 
+            this.luanDataSet.DataSetName = "LuanDataSet";
+            this.luanDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // funcionariosTableAdapter
+            // 
+            this.funcionariosTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mskTel);
+            this.Controls.Add(this.mskCep);
+            this.Controls.Add(this.lblUf);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.lblBairro);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtUf);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.funcionariosBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.luanDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbNovo;
+        private System.Windows.Forms.ToolStripButton tsbCancelar;
+        private System.Windows.Forms.ToolStripButton tsbSalvar;
+        private System.Windows.Forms.ToolStripButton tsbAlterar;
+        private System.Windows.Forms.ToolStripButton tsbExcluir;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox tstIdBuscar;
+        private System.Windows.Forms.ToolStripButton tsbPesquisar;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtUf;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.Label lblBairro;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.Label lblUf;
+        private System.Windows.Forms.MaskedTextBox mskCep;
+        private System.Windows.Forms.MaskedTextBox mskTel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private LuanDataSet luanDataSet;
+        private System.Windows.Forms.BindingSource funcionariosBindingSource;
+        private LuanDataSetTableAdapters.FuncionariosTableAdapter funcionariosTableAdapter;
+    }
+}
+
---
+++ Crud/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Crud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection sqlCon = null;
+        private string strCon = @"Persist Security Info=False;User ID=sa;Password=1234567; Initial Catalog=Luan;Data Source=(local)";
+        private string strSql = string.Empty;
+
+
+       private void tsbSalvar_Click(object sender, EventArgs e)
+        {
+            strSql = "insert into Funcionarios (Id, Nome, Endereco, CEP, Bairro, Cidade, UF, Telefone) values(@Id, @Nome, @Endereco, @CEP, @Bairro, @Cidade, @UF, @Telefone)";
+
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@Id", SqlDbType.Int).Value = txtId.Text;
+            comando.Parameters.Add("@Nome", SqlDbType.VarChar).Value = txtNome.Text;
+            comando.Parameters.Add("@Endereco", SqlDbType.VarChar).Value = txtEndereco.Text;
+            comando.Parameters.Add("@CEP", SqlDbType.VarChar).Value = mskCep.Text;
+            comando.Parameters.Add("@Bairro", SqlDbType.VarChar).Value = txtBairro.Text;
+            comando.Parameters.Add("@Cidade", SqlDbType.VarChar).Value = txtCidade.Text;
+            comando.Parameters.Add("@UF", SqlDbType.VarChar).Value = txtUf.Text;
+            comando.Parameters.Add("@Telefone", SqlDbType.VarChar).Value = mskTel.Text;
+
+
+
+            try
+            {
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Cadastro realizado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                sqlCon.Close();           
+            }
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbAlterar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tstIdBuscar.Enabled = true;
+            tsbPesquisar.Enabled = true;
+            txtNome.Enabled = false;
+            txtId.Enabled = false;
+            txtEndereco.Enabled = false;
+            txtCidade.Enabled = false;
+            txtBairro.Enabled = false;
+            mskCep.Enabled = false;
+            mskTel.Enabled = false;
+            txtUf.Enabled = false;
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            txtCidade.Text = "";
+            txtBairro.Text = "";
+            txtId.Text = "";
+            txtUf.Text = "";
+            mskCep.Text = "";
+            mskTel.Text = "";
+        }
+
+        private void tsbPesquisar_Click(object sender, EventArgs e)
+        {
+            strSql = "select * from Funcionarios where Id = @Id";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+
+            comando.Parameters.Add("@Id", SqlDbType.Int).Value = tstIdBuscar.Text;
+
+
+            try
+            {
+                if (tstIdBuscar.Text == string.Empty)
+                {
+                    throw new Exception("Voce precisa digitar um Id!!");
+                }
+                sqlCon.Open();
+
+                SqlDataReader dr = comando.ExecuteReader();
+
+                if (dr.HasRows == false)
+                {
+                    throw new Exception("ID nao cadastrado");
+                }
+                dr.Read();
+                txtId.Text = Convert.ToString(dr["Id"]);
+                txtNome.Text = Convert.ToString(dr["Nome"]);
+                txtEndereco.Text = Convert.ToString(dr["Endereco"]);
+                mskCep.Text = Convert.ToString(dr["CEP"]);
+                txtBairro.Text = Convert.ToString(dr["Bairro"]);
+                txtCidade.Text = Convert.ToString(dr["Cidade"]);
+                txtUf.Text = Convert.ToString(dr["UF"]);
+                mskTel.Text = Convert.ToString(dr["Telefone"]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+            tsbNovo.Enabled = false;
+            tsbSalvar.Enabled = false;
+            tsbAlterar.Enabled = true;
+            tsbCancelar.Enabled = true; 
+            tsbExcluir.Enabled = true;
+            tstIdBuscar.Enabled = true;
+            tsbPesquisar.Enabled = true;
+            txtNome.Enabled = true;
+            txtId.Enabled = true;
+            txtEndereco.Enabled = true;
+            txtCidade.Enabled = true;
+            txtBairro.Enabled = true;
+            mskCep.Enabled = true;
+            mskTel.Enabled = true;
+            txtUf.Enabled = true;
+            tsbPesquisar.Text = "";
+            txtNome.Focus();
+            ;        }
+
+        private void tsbAlterar_Click(object sender, EventArgs e)
+        {
+            strSql = "update Funcionarios set Id=@id, Nome=@Nome, Endereco=@Endereco, CEP=@CEP, Bairro=@Bairro, Cidade=@Cidade, UF=@UF, Telefone=@Telefone where id = @idBuscar";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+
+            comando.Parameters.Add("@IdBuscar", SqlDbType.Int).Value = tstIdBuscar.Text;
+
+            comando.Parameters.Add("@Id", SqlDbType.Int).Value = txtId.Text;
+            comando.Parameters.Add("@Nome", SqlDbType.VarChar).Value = txtNome.Text;
+            comando.Parameters.Add("@Endereco", SqlDbType.VarChar).Value = txtEndereco.Text;
+            comando.Parameters.Add("@CEP", SqlDbType.VarChar).Value = mskCep.Text;
+            comando.Parameters.Add("@Bairro", SqlDbType.VarChar).Value = txtBairro.Text;
+            comando.Parameters.Add("@Cidade", SqlDbType.VarChar).Value = txtCidade.Text;
+            comando.Parameters.Add("@UF", SqlDbType.VarChar).Value = txtUf.Text;
+            comando.Parameters.Add("@Telefone", SqlDbType.VarChar).Value = mskTel.Text;
+
+            try
+            {
+
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Cadastro atualizado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                sqlCon.Close();
+            }
+        }
+
+        private void tsbExcluir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente excluir este funcionario?" , "Cuidado", MessageBoxButtons.YesNo,MessageBoxIcon.Question,MessageBoxDefaultButton.Button2)== DialogResult.No)
+            {
+                MessageBox.Show("Operação cancelada!");            
+            }
+            else {
+                strSql = "delete from Funcionarios where Id=@Id";
+                sqlCon = new SqlConnection(strCon);
+                SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+
+                comando.Parameters.Add("@Id", SqlDbType.Int).Value = tstIdBuscar.Text;
+
+
+                try
+                {
+                    sqlCon.Open();
+                    comando.ExecuteNonQuery();
+                    MessageBox.Show("Funcionario deletado com sucesso!!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally {
+                    sqlCon.Close();
+                }
+                tsbNovo.Enabled = true;
+                tsbSalvar.Enabled = false;
+                tsbAlterar.Enabled = false;
+                tsbCancelar.Enabled = false;
+                tsbExcluir.Enabled = false;
+                tstIdBuscar.Enabled = true;
+                tsbPesquisar.Enabled = true;
+                txtNome.Enabled = false;
+                txtId.Enabled = false;
+                txtEndereco.Enabled = false;
+                txtCidade.Enabled = false;
+                txtBairro.Enabled = false;
+                mskCep.Enabled = false;
+                mskTel.Enabled = false;
+                txtUf.Enabled = false;
+                txtNome.Text = "";
+                txtEndereco.Text = "";
+                txtCidade.Text = "";
+                txtBairro.Text = "";
+                txtId.Text = "";
+                txtUf.Text = "";
+                mskCep.Text = "";
+                mskTel.Text = "";
+            }
+
+        }
+
+        private void tsbNovo_Click(object sender, EventArgs e)
+        {
+            tsbNovo.Enabled = false;
+            tsbSalvar.Enabled = true;
+            tsbAlterar.Enabled = false;
+            tsbCancelar.Enabled = true;
+            tsbExcluir.Enabled = true;
+            tstIdBuscar.Enabled = false;
+            txtNome.Enabled = true;
+            txtId.Enabled = true;
+            txtEndereco.Enabled = true;
+            txtCidade.Enabled = true;
+            txtBairro.Enabled = true;
+            mskCep.Enabled = true;
+            mskTel.Enabled = true;
+            txtUf.Enabled = true;
+
+        }
+
+        private void tsbCancelar_Click(object sender, EventArgs e)
+        {
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbAlterar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tstIdBuscar.Enabled = true;
+            tsbPesquisar.Enabled = true;
+            txtNome.Enabled = false;
+            txtId.Enabled = false;
+            txtEndereco.Enabled = false;
+            txtCidade.Enabled = false;
+            txtBairro.Enabled = false;
+            mskCep.Enabled = false;
+            mskTel.Enabled = false;
+            txtUf.Enabled = false;
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            txtCidade.Text = "";
+            txtBairro.Text = "";
+            txtId.Text = "";
+            txtUf.Text = "";
+            mskCep.Text = "";
+            mskTel.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'luanDataSet.Funcionarios' table. You can move, or remove it, as needed.
+            this.funcionariosTableAdapter.Fill(this.luanDataSet.Funcionarios);
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbAlterar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tstIdBuscar.Enabled = true;
+            tsbPesquisar.Enabled = true;
+            txtNome.Enabled = false;
+            txtId.Enabled = false;
+            txtEndereco.Enabled = false;
+            txtCidade.Enabled = false;
+            txtBairro.Enabled = false;
+            mskCep.Enabled = false;
+            mskTel.Enabled = false;
+            txtUf.Enabled = false;
+        }
+
+    }
+}
